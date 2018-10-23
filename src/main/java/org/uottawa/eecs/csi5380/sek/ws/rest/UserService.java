b00fa69e2bdaf8ad0f4a2e1c0639c74f861d65f5
unknown <kghosh.ns@gmail.com>
2018-10-23 01:24:04 -0400

    complete support for user sign in / out

--- src/main/java/org/uottawa/eecs/csi5380/sek/ws/rest/UserService.java
+++ src/main/java/org/uottawa/eecs/csi5380/sek/ws/rest/UserService.java
@@ -1,5 +1,7 @@
 package org.uottawa.eecs.csi5380.sek.ws.rest;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.ws.rs.Consumes;
@@ -11,63 +13,85 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 import org.uottawa.eecs.csi5380.sek.model.User;
 import org.uottawa.eecs.csi5380.sek.utils.DBUtils;
 
 @Path("/user_service")
 @Produces(MediaType.APPLICATION_JSON)
-//@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 public class UserService {
-	
+
 	private DBUtils dbUtil;
 	HttpSession session;
-	
+
 	public UserService(@Context HttpServletRequest request) {
 		this.dbUtil = new DBUtils();
 		this.session = request.getSession(true);
 	}
-	
+
 	@POST
 	@Path("/signin")
-	public Response.Status signIn(@FormParam("user_name") String user_name,
-            @FormParam("password") String password) {
-		User user = dbUtil.createEm().createQuery("SELECT t FROM User t WHERE t.bookid = :bookid", User.class)
-		.setParameter("user_name", user_name)
-		.setParameter("password", password)
-		.getResultList().get(0);
-		if(user != null) {
-			session.setAttribute("signedInUser","user");
+	public Response signIn(@FormParam("user_name") String user_name, @FormParam("password") String password)
+			throws JSONException {
+		List<User> user = dbUtil.createEm()
+				.createQuery("SELECT t FROM User t WHERE t.userName = :user_name AND t.password = :password",
+						User.class)
+				.setParameter("user_name", user_name).setParameter("password", password).getResultList();
+		if (!user.isEmpty()) {
+			session.setAttribute("signedInUser", user.get(0));
+			return Response.status(Status.OK).entity(new JSONObject().put("msg", "Successful")).build();
+		} else {
+			return Response.status(Status.OK).entity(new JSONObject().put("msg", "Sign in unsuccessful")).build();
 		}
-		return Response.Status.OK;
 	}
-	
+
 	@POST
 	@Path("/create_account")
-	public Response.Status createAccount(@FormParam("user_name") String user_name,
-            @FormParam("email") String email, @FormParam("password") String password, 
-            @FormParam("first_name") String first_name, @FormParam("last_name") String last_name) {
-		try {
-		User aUser = new User();
-		aUser.setUserName(user_name);
-		aUser.setEmail(email);
-		aUser.setFirstName(first_name);
-		aUser.setLastName(last_name);
-		dbUtil.createEm().getTransaction().begin();
-		dbUtil.createEm().persist(aUser);
-		dbUtil.createEm().getTransaction().commit();
-		dbUtil.createEm().close();
-		}catch(Exception e) {
-			System.out.println("*************************************" + e);
+	public Response createAccount(@FormParam("user_name") String user_name, @FormParam("email") String email,
+			@FormParam("password") String password, @FormParam("first_name") String first_name,
+			@FormParam("last_name") String last_name) throws JSONException {
+
+		List<User> user = dbUtil.createEm()
+				.createQuery("SELECT t FROM User t WHERE t.userName = :user_name", User.class)
+				.setParameter("user_name", user_name)
+				.getResultList();
+		if (!user.isEmpty()) {
+			return Response.status(Status.OK).entity(new JSONObject().put("msg", "username has already been taken"))
+					.build();
+		} else {
+			try {
+				User aUser = new User();
+				aUser.setUserName(user_name);
+				aUser.setEmail(email);
+				aUser.setPassword(password);
+				aUser.setFirstName(first_name);
+				aUser.setLastName(last_name);
+				dbUtil.createEm().getTransaction().begin();
+				dbUtil.createEm().persist(aUser);
+				dbUtil.createEm().getTransaction().commit();
+				dbUtil.createEm().close();
+			} catch (Exception e) {
+				System.out.println("*************************************" + e);
+			}
+			return Response.status(Status.CREATED)
+					.entity(new JSONObject().put("msg", "account successfully created!")).build();
 		}
-		return Response.Status.OK;
 	}
-	
+
+	@GET
+	@Path("/is_signed_in")
+	public boolean isSignedIn() {
+		return session.getAttribute("signedInUser") != null;
+	}
+
 	@GET
 	@Path("/signout")
-	public void signOut() {
-		
+	public Response signOut() throws JSONException {
+		session.invalidate();
+		return Response.status(Status.OK).entity(new JSONObject().put("msg", "Sign out successful")).build();
 	}
 
 }
-
